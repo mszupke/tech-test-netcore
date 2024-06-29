d8fce00c0d9998f6824cac88e6322932d49e61cb
Marcin Szupke <marcin.szupke@gmail.com>
2024-06-29 08:02:45 +0200

    #2 sort todo items by importance

--- Todo/EntityModelMappers/TodoLists/TodoListDetailViewmodelFactory.cs
+++ Todo/EntityModelMappers/TodoLists/TodoListDetailViewmodelFactory.cs
@@ -10,6 +10,10 @@ namespace Todo.EntityModelMappers.TodoLists
         public static TodoListDetailViewmodel Create(TodoList todoList)
         {
             var items = todoList.Items.Select(TodoItemSummaryViewmodelFactory.Create).ToList();
+
+            // Server side sorting
+            // var items = todoList.Items.OrderBy(x => x.Importance).Select(TodoItemSummaryViewmodelFactory.Create).ToList();
+            
             return new TodoListDetailViewmodel(todoList.TodoListId, todoList.Title, items);
         }
     }
