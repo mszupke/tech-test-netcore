72d5d33a15fafe39a330ada0ee02523ca8368ae8
Marcin Szupke <marcin.szupke@gmail.com>
2024-06-29 10:41:46 +0200

    Add Rank property to TodoItem class and related functionality

--- Todo/Data/Migrations/20240629074946_AddTodoItemRankProperty.Designer.cs
+++ Todo/Data/Migrations/20240629074946_AddTodoItemRankProperty.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Todo.Data;
 
@@ -10,9 +11,11 @@ using Todo.Data;
 namespace Todo.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240629074946_AddTodoItemRankProperty")]
+    partial class AddTodoItemRankProperty
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
