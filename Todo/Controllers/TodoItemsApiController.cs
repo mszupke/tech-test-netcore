c89e68583bb57ad236aa8b11dcb034240fc286dc
Marcin Szupke <marcin.szupke@gmail.com>
2024-07-04 22:51:37 +0200

    #10 change rank using buttons, add API patch method to update rank

---
+++ Todo/Controllers/TodoItemsApiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Todo.Data;
+
+namespace Todo;
+
+[Route("/api/todo-items")]
+[ApiController]
+public class TodoItemsApiController : ControllerBase
+{
+    private readonly ApplicationDbContext dbContext;
+
+    public TodoItemsApiController(ApplicationDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    [HttpPatch("update-rank")]
+    public async Task<IActionResult> UpdateRank([FromBody] UpdateRankModel model)
+    {
+        var item = await dbContext.TodoItems.FindAsync(model.Id);
+        if (item == null)
+        {
+            return NotFound();
+        }
+
+        item.Rank = model.Rank;
+        await dbContext.SaveChangesAsync();
+
+        return Ok(item);
+    }
+}
---
+++ Todo/Models/TodoItems/UpdateRankModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Todo;
+
+public class UpdateRankModel
+{
+    public int Id { get; set; }
+    public int Rank { get; set; }
+}
